1c0e3c582bf2a41ccc464a590e14a72c29c957cb
RichieQuintana <103153905+RichieQuintana@users.noreply.github.com>
2024-07-18 21:31:09 -0500

    La implementación de producer y consumer scripts para RabbitMQ
    Estos scripts establecen la configuración básica de comunicación entre diferentes partes de la plataforma Travelix, que permiten el paso de mensajes asincrónicos y procesamiento de tareas. Esta integración es crucial para manejar varios aspectos de la gestión de viajes como reservas, cancelaciones y actualizaciones de manera eficiente y escalable.

---
+++ rabbitMQ/consumer.py
@@ -0,0 +1,15 @@
+import pika
+
+credentials = pika.PlainCredentials('user', 'password')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body}")
+
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
