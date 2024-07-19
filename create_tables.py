c29a9587b5a03058da06a84e5d21105796df8572
Alex_Caicedo_R384 <131833190+Alex-Caicedo-R384@users.noreply.github.com>
2024-07-18 23:58:14 -0500

    Codigo para cread tablas
    este codigo se utilizo para crear las tablas en la base de datos , para no hacerlo desde el ssms o desde el query editor de azure

---
+++ create_tables.py
@@ -0,0 +1,12 @@
+# create_tables.py
+# from app.config import Config
+# from infrastructure.databases.connection import Base, engine
+# from app.models.user import User
+# from app.models.user_flight import UserFlight
+# from app.models.flight import Flight
+# from app.models.flight_assignment import FlightAssignment
+
+# Crear todas las tablas en la base de datos
+# Base.metadata.create_all(bind=engine)
+
+# print("Las tablas se han creado correctamente en la base de datos.")
