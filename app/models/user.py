5a754214b0967f646ce386d3408fc9813fac7922
Alex_Caicedo_R384 <131833190+Alex-Caicedo-R384@users.noreply.github.com>
2024-07-18 23:52:38 -0500

    Update user.py
    relación con flight

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,14 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from infrastructure.databases.connection import Base
 
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
+    name = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True, index=True) 
     hashed_password = Column(String)
+
+    # Relación con los vuelos de los usuarios
+    flights = relationship('UserFlight', back_populates='user')
