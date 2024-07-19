3820d985e6342c3f8310930b8a3479e33ec46b6f
Alex_Caicedo_R384 <131833190+Alex-Caicedo-R384@users.noreply.github.com>
2024-07-18 23:53:30 -0500

    URL con la bdd de userflight

--- app/config.py
+++ app/config.py
@@ -1,13 +1,15 @@
+from infrastructure.databases.user import user, password 
+
 class Config:
-    # Aquí puedes definir variables de configuración para tu aplicación
     APP_NAME = "Travelix"
     VERSION = "1.0.0"
     DEBUG = True
 
-    # Configuraciones de la base de datos
-    DATABASE_URL = "sqlite:///./test.db"  # Ejemplo de URL de una base de datos SQLite
+    DATABASE_URL = (
+        f"mssql+pyodbc://{user}:{password}@travelixdb.database.windows.net:1433/"
+        "userflight?driver=ODBC+Driver+17+for+SQL+Server"
+    )
 
-    # Configuraciones de JWT u otros middleware que se necesiten
     JWT_SECRET_KEY = "your-secret-key"
     JWT_ALGORITHM = "HS256"
     JWT_EXPIRATION_TIME_MINUTES = 30
