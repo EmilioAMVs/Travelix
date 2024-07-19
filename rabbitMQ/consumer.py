a086813a0b7a1fdec5e2eb2a65c85dd5fa8b7cf0
RichieQuintana <103153905+RichieQuintana@users.noreply.github.com>
2024-07-19 17:38:12 -0500

    esta conectada con la API

--- rabbitMQ/consumer.py
+++ rabbitMQ/consumer.py
@@ -1,15 +1,28 @@
 import pika
+import smtplib
+from email.mime.text import MIMEText
 
-credentials = pika.PlainCredentials('user', 'password')
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
-channel = connection.channel()
+def send_email(to_address, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = 'r20003848@gmail.com'
+    msg['To'] = to_address
 
-channel.queue_declare(queue='hello')
+    with smtplib.SMTP('smtp.example.com', 587) as server:
+        server.starttls()
+        server.login('r20003848@gmail.com', 'jcsb jdks whbe opdn')
+        server.sendmail('r20003848@gmail.com', to_address, msg.as_string())
 
 def callback(ch, method, properties, body):
-    print(f" [x] Received {body}")
+    reservation_details = body.decode()
+    # Procesar los detalles de la reserva y enviar correo electrónico
+    send_email('destinatario@example.com', 'Detalles de tu reserva', reservation_details)
 
-channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+credentials = pika.PlainCredentials('administrator', 'password')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
+channel = connection.channel()
+channel.queue_declare(queue='reservations')
+channel.basic_consume(queue='reservations', on_message_callback=callback, auto_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
+print('Esperando mensajes...')
 channel.start_consuming()
